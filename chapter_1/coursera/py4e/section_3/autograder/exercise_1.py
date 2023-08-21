da427616db0bea19e30188bb46daeacf803b0d3f
Andrii Vasylenko <83074078+AndriiVasylenko-bit@users.noreply.github.com>
2023-08-21 20:03:53 +0300

    coursera\py4e\section_3\autograder\exercise_1.py

--- chapter_1/coursera/py4e/section_3/autograder/exercise_1.py
+++ chapter_1/coursera/py4e/section_3/autograder/exercise_1.py
@@ -1,11 +1,15 @@
 # 3.1 Write a program to prompt the user for hours and rate per hour using input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
 # 3.1 Написать программу, чтобы подсказать пользователю часы и тариф в час, используя ввод для расчета валовой оплаты. Оплата почасовой ставки за часы до 40 часов и 1,5 почасовой ставки за все часы работы свыше 40 часов. Используйте 45 часов и тариф 10,50 в час для тестирования программы (плата должна быть 498,75). Вы должны использовать ввод для чтения строки и float() для преобразования строки в число. Не беспокойтесь об ошибке проверки ввода пользователем - предположите, что номера пользователей правильно.
 hrs = input('Enter Hours:')
-h = float(hrs)
 rph = input('Enter the rate per hour/ставку в час:')
-r = float(rph)
-pay = 0
+try:
+    h = float(hrs)
+    r = float(rph)
+    pay = 0
 
+except:
+    print('Error, please enter numeric input')
+    quit() # Прекратить.
 if h > 40 :
     pay = 40 * r + (h-40)*(1.5*10.5)
 else:
@@ -13,7 +17,7 @@ else:
 print(pay)
 
 
- #  105
+#  105
  #   15
  #
  #   75
--- chapter_1/lab/test.py
+++ chapter_1/lab/test.py
@@ -1,6 +1,5 @@
-astr = 'Hello Bob'
-istr = int(astr) # ValueError
-print('First', istr)
-astr = '123'
-istr = int(astr)
-print('Second', istr)
+big = max('Hello world')
+print(big)
+
+tiny = min('Hello world') #маленький
+print(tiny)
