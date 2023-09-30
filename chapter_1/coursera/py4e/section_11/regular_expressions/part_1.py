b498c83a5be7e70ce3f8239af1aa3f31faa25fa5
Andrii Vasylenko <83074078+AndriiVasylenko-bit@users.noreply.github.com>
2023-09-30 08:36:01 +0300

    coursera\py4e\section_11\regular_expressions\part_1.py

--- chapter_1/coursera/py4e/section_11/regular_expressions/part_1.py
+++ chapter_1/coursera/py4e/section_11/regular_expressions/part_1.py
@@ -8,3 +8,14 @@ for line in hand:
     line = line.rstrip()
     if re.search('From:', line):
         print(line)
+
+# ^ - Соответствовать началу строки
+
+hand = open('mbox-short.txt')
+for line in hand:
+    line = line.rstrip()
+    if re.search('^X.*:', line):
+        print(line)
+
+# * - во многих случаях
+# . - Соответствовать любому символу
