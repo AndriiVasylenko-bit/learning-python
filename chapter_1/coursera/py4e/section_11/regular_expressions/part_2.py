4dfdf44572d2f52586424d8c8225665debcd4168
Andrii Vasylenko <83074078+AndriiVasylenko-bit@users.noreply.github.com>
2023-10-27 23:54:26 +0300

    py4e\section_11\regular_expressions\part_2.py

--- chapter_1/coursera/py4e/section_11/regular_expressions/part_2.py
+++ chapter_1/coursera/py4e/section_11/regular_expressions/part_2.py
@@ -1,5 +1,7 @@
 import re
-
+# [0-9]+ One ro more digits
 x = 'My 2 favorite numbers are 19 and 42'
 y = re.findall('[0-9]+', x)
+print(y)
+y = re.findall('[AEIOU]+', x) # One or more uppercase letters
 print(y)
