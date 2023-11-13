9e655967a2fb3e90a3b2d0bdce4d23f42a917d0e
Andrii Vasylenko <83074078+AndriiVasylenko-bit@users.noreply.github.com>
2023-11-13 22:41:18 +0200

    chapter_1\lab\test.py

--- chapter_1/lab/test.py
+++ chapter_1/lab/test.py
@@ -1,17 +1,22 @@
-import re
+import requests
+from bs4 import BeautifulSoup
 
-line = """"Ветаптека Липова Долина • Ветаптека У Липовій Долині • Ветаптеки Липова
-        Долина • Ветаптеки У Липовій Долині • Ветеринарні Ліки • Вітаміни Для
-        Тварин • Зоомедикація • Ветеринарні Препарати • Лікування Тварин •
-        Допомога Ветеринара • Ветеринарні Консультації • Аптека Для Тварин •
-        Профілактика Захворювань У Тварин • Ветеринарні Рекомендації • Ветаптека
-        В Липовій Долині • Зоотовари Для Тварин • Товари Для Ветмедицини •
-        Ветеринарна Аптека • Ветеринарні Клініки • Ветеринар • Ветеринарна
-        Клініка • Ветеринарний Кабінет • Ветеринарні Кабінети • Ветеринарія •
-        Ветеринарні Послуги • Все Для Тварин • Все Для Бджільництва •
-        Акваріумістика • Ветпрепарати.
-"""
-# Используйте регулярное выражение для удаления \n после точек
-line = re.sub(r' •', r',', line)
+url = "https://trends.rbc.ru/trends/industry/5e845cec9a794747bf03e2c9"
 
-print(line)
+# Send a GET request to the URL
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the element with class "article__text__main"
+    article_text_main = soup.find('div', class_='article__text__main')
+
+    # Extract and print the text within the <p> tags
+    paragraphs = article_text_main.find_all('p')
+    for paragraph in paragraphs:
+        print(paragraph.get_text().strip())
+else:
+    print(f"Error: Unable to retrieve the content. Status Code: {response.status_code}")
