57f64acc60bb89b2a04c7fd9652dc12b590c6bf4
Andrii Vasylenko <83074078+AndriiVasylenko-bit@users.noreply.github.com>
2023-11-25 21:27:25 +0200

    chapter_1\lab\cycles\codewars

--- chapter_1/lab/cycles/codewars.py
+++ chapter_1/lab/cycles/codewars.py
@@ -1,10 +1,15 @@
 # For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 def count_positives_sum_negatives(arr):
-    if arr is not None:
-        lst = list()
-        b = True
-        for i in range(len(arr)):
-            if arr[i] < 0 and b:
-                lst.append(i-1)
-                b = False
+    if not arr: return []
+    pos = 0
+    neg = 0
+    for x in arr:
+        if x > 0:
+            pos += 1
+        if x < 0:
+            neg += x
+    return [pos, neg]
+
+
+print(count_positives_sum_negatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]))
