e28ddc5a63941d6510e901efbb375da661580ce9
AndriiVasylenko-bit <livekidos95@gmail.com>
2024-01-07 23:21:32 +0200

    Added folder try

---
+++ section_15/autograder/counting_email_database/Try/emaildb.py
@@ -0,0 +1,35 @@
+import sqlite3, re
+
+debag = True
+
+dic = dict()
+fname = ''
+if not debag:
+    fname = input('Enter file name: ')
+if (len(fname) < 1):
+    fname = 'mbox.txt'
+fh = open(fname)
+for line in fh:
+    if not line.startswith('From: '):
+        continue
+    pieces = line.split()
+    org = pieces[1]
+
+    domian = re.search(r'@(.+)$', org).group(1)
+    dic[domian] = dic.get(domian, 0) + 1
+lst = sorted([(v, k) for (k, v) in dic.items()])
+for (k, v) in lst: print(k, v)
+
+conn = sqlite3.connect('email_count2.sqlite')
+cur = conn.cursor()
+
+cur.execute('DROP TABLE IF EXISTS Counts')
+cur.execute('''
+CREATE TABLE Counts(org TEXT, count INTEGER)''')
+
+row = cur.fetchone()
+for (v, k) in lst:
+    cur.execute('INSERT INTO Counts (org, count) VALUES (?, ?)',
+                (k, v,))
+    conn.commit()
+cur.close()
--- section_15/autograder/counting_email_database/emaildb.py
+++ section_15/autograder/counting_email_database/emaildb.py
@@ -3,47 +3,33 @@ import sqlite3, re
 debag = True
 
 
-conn = sqlite3.connect('orgdb.sqlite')
+conn = sqlite3.connect('email_count2.sqlite')
 cur = conn.cursor()
 
-dic = dict()
+cur.execute('DROP TABLE IF EXISTS Counts')
+
+cur.execute('''
+CREATE TABLE Counts (org TEXT, count INTEGER)''')
+
 fname = ''
-if not debag:
-    fname = input('Enter file name: ')
-if (len(fname) < 1):
-    fname = 'mbox.txt'
+if not debag: fname = input('Enter file name: ')
+if (len(fname) < 1): fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
-    if not line.startswith('From: '):
-        continue
+    if not line.startswith('From: '): continue
     pieces = line.split()
     org = pieces[1]
+    cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
+    row = cur.fetchone()
+    if row is None:
+        cur.execute('''INSERT INTO Counts (org, count)
+                VALUES (?, 1)''', (org,))
+    else:
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
+                    (org,))
+conn.commit()
 
-    domian = re.search(r'@(.+)$', org).group(1)
-    dic[domian] = dic.get(domian, 0) + 1
-
-lst = sorted([(v, k) for (k, v) in dic.items()])
-for (k, v) in lst: print(k, v)
-
-
-
-
-# cur.execute('DROP TABLE IF EXISTS Counts')
-#
-# cur.execute('''
-# CREATE TABLE Counts (org TEXT, count INTEGER)''')
-#
-#     cur.execute('SELECT count FROM Counts WHERE org = ? ', (org,))
-#     row = cur.fetchone()
-#     if row is None:
-#         cur.execute('''INSERT INTO Counts (org, count)
-#                 VALUES (?, 1)''', (org,))
-#     else:
-#         cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',
-#                     (org,))
-# conn.commit()
-#
-# # https://www.sqlite.org/lang_select.html
+# https://www.sqlite.org/lang_select.html
 # sqlstr = 'SELECT org, count FROM Counts ORDER BY count'
 #
 # dic = dict()
@@ -54,9 +40,9 @@ for (k, v) in lst: print(k, v)
 #
 # lst = sorted([(v, k) for (k, v) in dic.items()])
 # for (k, v) in lst: print(k, v)
-#
-# # print('Sum org: ', sum(count))
-# # print('Max org: ', max(count))
-#
-#
-# cur.close()
+
+# print('Sum org: ', sum(count))
+# print('Max org: ', max(count))
+
+
+cur.close()
